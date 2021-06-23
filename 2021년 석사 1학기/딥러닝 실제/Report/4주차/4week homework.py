634a503f89960e7aad2dc09d9461d1c99ec454ab
woosangjin <sang9843@naver.com>
2021-06-23 15:30:06 +0900

    Add files via upload

---
+++ 2021년 석사 1학기/딥러닝 실제/Report/4주차/4week homework.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('C://wsj/wsj.jpg')
+
+plt.imshow(img)
+plt.show
+
+
+# In[ ]:
+
+
+
+
